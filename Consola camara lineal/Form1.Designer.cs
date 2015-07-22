9c414fafc1e21545617ac63fd8179ddb1ec85153
gilrieke <griekejr@gmail.com>
2015-07-22 16:20:03 -0500

    Added Files

---
+++ Consola camara lineal/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Consola_camara_lineal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.camera_values = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.camera_values)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // camera_values
+            // 
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.LabelStyle.Enabled = false;
+            chartArea1.AxisX.Maximum = 128D;
+            chartArea1.AxisX.Minimum = 1D;
+            chartArea1.AxisY.Interval = 16D;
+            chartArea1.AxisY.Maximum = 256D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.camera_values.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.camera_values.Legends.Add(legend1);
+            this.camera_values.Location = new System.Drawing.Point(35, 283);
+            this.camera_values.Name = "camera_values";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "raw";
+            this.camera_values.Series.Add(series1);
+            this.camera_values.Size = new System.Drawing.Size(1281, 530);
+            this.camera_values.TabIndex = 0;
+            this.camera_values.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1405, 853);
+            this.Controls.Add(this.camera_values);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.camera_values)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart camera_values;
+    }
+}
+
